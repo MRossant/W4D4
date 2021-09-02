8008451e90348b29d5dfdbc045ea8f7bf00ab31d
AdeshPawaroo <adeshpawaroo1@gmail.com>
2021-09-02 13:06:57 -0400

    started

---
+++ rspec_practice/spec/problems_rspec.rb
@@ -0,0 +1,12 @@
+require "problems"
+
+describe Array do
+    
+    subject(:array) { Array.new }
+
+    describe "#my_uniq" do
+        it "removes duplicates from an array" do
+            expect()
+        end
+    end
+end
