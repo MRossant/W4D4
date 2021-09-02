9602e6b00bab66ffc88e501897513eb56d29d601
AdeshPawaroo <adeshpawaroo1@gmail.com>
2021-09-02 14:48:29 -0400

    filled specs

--- rspec_practice/lib/problems.rb
+++ rspec_practice/lib/problems.rb
@@ -1,12 +1,14 @@
 class Array
-
+   
     def my_uniq
+        raise "array is empty" if self.empty?
         result = []
         self.each {|el| result << el if !result.include?(el)}
         result
     end
-
+    
     def two_sum
+        raise "array is empty" if self.empty?
         result = []
         (0...self.length).each do |i|
             (i + 1...self.length).each do |j|
--- rspec_practice/spec/problems_spec.rb
+++ rspec_practice/spec/problems_spec.rb
@@ -7,11 +7,36 @@ describe "#my_uniq" do
     it "removes duplicates from an array" do
         expect(array.my_uniq).to eq([1,2,3])
     end
+
+    it "raises an error if the array is empty" do
+        expect {[].my_uniq}.to raise_error("array is empty")
+    end
+
+    it "should not modify the original" do
+        duped_arr = array.dup
+        duped_arr.my_uniq
+        expect(duped_arr).to eq(array)
+    end
 end
 
 describe "#two_sum" do
     let(:array) { [-1,0,2,-2,1] }
+
     it "returns pair indices that add up to zero" do
         expect(array.two_sum).to eq([[0,4], [2,3]])
     end
+
+    it "raises an error if the array is empty" do
+        expect {[].two_sum}.to raise_error("array is empty")
+    end
+
+    it "should not modify the original" do
+        duped_arr = array.dup
+        duped_arr.two_sum
+        expect(duped_arr).to eq(array)
+    end
+end
+
+describe "#my_transpose" do
+    
 end
