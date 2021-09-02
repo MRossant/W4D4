6439d6b8dfc1f86eed184f894cf3df9f4a47e252
mrossant <matteorossant@gmail.com>
2021-09-02 14:21:36 -0400

    Finished two_sum

--- rspec_practice/lib/problems.rb
+++ rspec_practice/lib/problems.rb
@@ -6,5 +6,13 @@ class Array
         result
     end
 
-    def 
+    def two_sum
+        result = []
+        (0...self.length).each do |i|
+            (i + 1...self.length).each do |j|
+                result << [i, j] if self[i] + self[j] == 0
+            end
+        end
+        result
+    end
 end
--- rspec_practice/spec/problems_spec.rb
+++ rspec_practice/spec/problems_spec.rb
@@ -8,3 +8,10 @@ describe "#my_uniq" do
         expect(array.my_uniq).to eq([1,2,3])
     end
 end
+
+describe "#two_sum" do
+    let(:array) { [-1,0,2,-2,1] }
+    it "returns pair indices that add up to zero" do
+        expect(array.two_sum).to eq([[0,4], [2,3]])
+    end
+end
